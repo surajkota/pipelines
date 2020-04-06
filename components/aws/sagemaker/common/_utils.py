a1980fa72f4372517246d296d0f76f0632777451
Suraj Kota <surakota@amazon.com>
2020-04-06 19:28:27 +0530

    fixes and user_agent

--- components/aws/sagemaker/common/_utils.py
+++ components/aws/sagemaker/common/_utils.py
@@ -19,7 +19,7 @@ import random
 import json
 import yaml
 
-import boto3
+import boto3, botocore
 from botocore.exceptions import ClientError
 from sagemaker.amazon.amazon_estimator import get_image_uri
 
@@ -52,13 +52,22 @@ built_in_algos = {
 # Get current directory to open templates
 __cwd__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
+def nullable_string_argument(value):
+    value = value.strip()
+    if not value:
+        return None
+    return value
+
 def add_default_client_arguments(parser):
     parser.add_argument('--region', type=str.strip, required=True, help='The region where the training job launches.')
-    parser.add_argument('--endpoint_url', type=str.strip, required=False, help='The URL to use when communicating with the Sagemaker service.')
+    parser.add_argument('--endpoint_url', type=nullable_string_argument, required=False, help='The URL to use when communicating with the Sagemaker service.')
 
 def get_sagemaker_client(region, endpoint_url=None):
     """Builds a client to the AWS SageMaker API."""
-    client = boto3.client('sagemaker', region_name=region, endpoint_url=endpoint_url)
+    session_config = botocore.config.Config(
+        user_agent='sagemaker-on-kubeflow-pipelines'
+    )
+    client = boto3.client('sagemaker', region_name=region, endpoint_url=endpoint_url, config=session_config)
     return client
 
 def create_training_job_request(args):
